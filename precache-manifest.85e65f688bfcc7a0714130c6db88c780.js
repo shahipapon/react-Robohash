14bac3f4ec7a1327b9453b52646a471171de6185
Shahi Shohanur Hamid <shahipapon@rocketmail.com>
2023-08-25 01:35:16 +0600

    Updates

---
+++ precache-manifest.85e65f688bfcc7a0714130c6db88c780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987c9f5f6a4bd31194826e02b8b719ce",
+    "url": "/reactrobo/index.html"
+  },
+  {
+    "revision": "d656981b702c1d87fad7",
+    "url": "/reactrobo/static/css/main.5c93efba.chunk.css"
+  },
+  {
+    "revision": "1e02a9fa46796fa6c62a",
+    "url": "/reactrobo/static/js/2.09d95117.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactrobo/static/js/2.09d95117.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d656981b702c1d87fad7",
+    "url": "/reactrobo/static/js/main.3fdad60d.chunk.js"
+  },
+  {
+    "revision": "ca2d85f0c5c4b561f2b8",
+    "url": "/reactrobo/static/js/runtime-main.cc00cd23.js"
+  }
+]);
